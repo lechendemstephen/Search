2924c1b27693ced579427d7b41eb33bb05ba4049
lechendemstephen <51188322+lechendemstephen@users.noreply.github.com>
2024-08-06 00:22:19 -0400

    other search functionalities

--- Todo/views.py
+++ Todo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Q
 
+from django.contrib import messages 
 from .models import Students
 # Create your views here.
 
@@ -21,9 +22,9 @@ def search_student(request):
         # getting the search quuery from the user using standard Django forms
         search_query = request.POST['search_query']
         # writing result of the search quarell based on search input 
-
         result = Students.objects.filter(Q(first_name__icontains=search_query) | Q(last_name__icontains=search_query))   
         context = {
+            'query': search_query,
             'students': student,
             'results':result
         }
